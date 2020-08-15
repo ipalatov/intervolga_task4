f45e1aec7e2ede6b4fdae1f0023f7d8b9e2286ab
Ivan <ivan.s.palatov@gmail.com>
2020-08-15 19:36:15 +0400

    config

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ define('DB_USER', 'ivan');
 define('DB_PASSWORD', '2222');
 
 // указать APP_ROOT относительно корня - если корневая директория находится не в корне хоста
-define('APP_ROOT', '/php-test/intervolga/task4/');
+define('APP_ROOT', '/');
 define('APP_MAIN', APP_ROOT . 'main/index');
