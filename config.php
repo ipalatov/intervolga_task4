84f36d5c2bb07dd74e0de3b1d9bfad718afef123
Ivan <ivan.s.palatov@gmail.com>
2020-08-18 19:59:02 +0400

    correct config comment

--- config.php
+++ config.php
@@ -7,5 +7,6 @@ define('DB_USER', 'ivan');
 define('DB_PASSWORD', '2222');
 
 // указать APP_ROOT относительно корня - если корневая директория находится не в корне хоста
+// (либо создать виртуальный хост apache)
 define('APP_ROOT', '/');
 define('APP_MAIN', APP_ROOT . 'main/index');
